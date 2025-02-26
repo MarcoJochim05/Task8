229a317300ff9e5a26cbc3f0039c02f013302e6e
MarcoJochim05 <marco.jochim@student.hnu.de>
2025-02-26 04:40:02 +0100

    Yeah

---
+++ app/src/main/java/com/example/task8/viewmodel/viewmodel.kt
@@ -0,0 +1,28 @@
+package com.example.task8.viewmodel
+
+import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.task8.model.Todo
+import com.example.task8.model.TodosApi
+import kotlinx.coroutines.launch
+class TodoViewModel: ViewModel() {
+    var todos = mutableStateListOf<Todo>()
+        private set
+    init {
+        getTodosList()
+    }
+    private fun getTodosList() {
+        viewModelScope.launch {
+            var todosApi: TodosApi? = null
+            try {
+                todosApi = TodosApi.getInstance()
+                todos.clear()
+                todos.addAll(todosApi.getTodos())
+            } catch (e: Exception) {
+                Log.d("ERROR",e.message.toString())
+            }
+        }
+    }
+}
